8b6c91fe7d717940970170855343c61f76437898
antoinevdm <antoine_vdm@hotmail.fr>
2017-02-22 15:12:02 +0100

    Refactoring API controllers

--- src/FirstBundle/Controller/APINoteController.php
+++ src/FirstBundle/Controller/APINoteController.php
@@ -4,15 +4,11 @@ namespace FirstBundle\Controller;
 
 use FirstBundle\Entity\note;
 use FirstBundle\Entity\categorie;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class APINoteController extends Controller
